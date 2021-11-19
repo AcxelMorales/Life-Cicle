3f3a94f4070b905c84a0db041b6b8ec76eb5bbb2
Acxel Morales <acxelmorales97@gmail.com>
2021-11-18 20:36:45 -0600

    Added example

--- app/src/main/java/com/example/lifecicle/MainActivity.kt
+++ app/src/main/java/com/example/lifecicle/MainActivity.kt
@@ -1,60 +1,26 @@
 package com.example.lifecicle
 
+import android.content.Intent
+
 import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
 
 import android.widget.Button
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-
-    var name = "Java"
-    val NAME = "Name"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.button)
-
-        //Toast.makeText(this, name, Toast.LENGTH_SHORT).show()
+        val button = findViewById<Button>(R.id.btnGoB)
 
         button.setOnClickListener {
-            this.name = "Kotlin"
-            Toast.makeText(this, name, Toast.LENGTH_SHORT).show()
+            var intent = Intent(this, ActivityB::class.java)
+            intent.putExtra("MESSAGE", "Hacia actividad B")
+            startActivity(intent)
         }
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        this.name = savedInstanceState.getString(this.NAME)!!
-
-        Toast.makeText(this, name, Toast.LENGTH_SHORT).show()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putString(this.NAME, this.name)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Toast.makeText(this, "En transiciòn", Toast.LENGTH_SHORT).show()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Toast.makeText(this, "App visible", Toast.LENGTH_SHORT).show()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Toast.makeText(this, "App oculta", Toast.LENGTH_SHORT).show()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Toast.makeText(this, "App destruida", Toast.LENGTH_SHORT).show()
     }
 
 }
